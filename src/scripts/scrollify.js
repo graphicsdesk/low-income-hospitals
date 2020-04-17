e071a941f56a9bca43341d608cb04b8f64201f7a
HongSenDu <hongsendu@gmail.com>
2020-04-17 11:14:28 -0400

    pls

--- src/scripts/scrollify.js
+++ src/scripts/scrollify.js
@@ -3,11 +3,26 @@ import scrollify from "jquery-scrollify";
 import { select } from "d3-selection";
 import { selectAll } from "d3-selection";
 import "d3-transition";
+var $ = require('jquery');
+
 
 var curr = 0;
 var onPart = 0;
 var canGo = true;
-var $ = require('jquery');
+var delay = 500;
+var delay2 = 1500;
+var loading = false;
+let touchstartX = 0;
+let touchstartY = 0;
+let touchendX = 0;
+let touchendY = 0;
+
+if (window.screen.width < 750) {
+    delay = 1500;
+    delay2 = 4000;
+}
+
+
 $(function () {
     $.scrollify({
         section: ".snap",
@@ -22,194 +37,76 @@ $(function () {
         updateHash: true,
         touchScroll: true,
         before: function (index, sections) { before(index, sections) },
-        after: function (index, sections) { checkInstant(index, sections); curr = index },
+        after: function (index, sections) { after(index, sections); curr = index },
         afterResize: function () { },
         afterRender: function () { }
     });
 });
-var delay = 500;
-var delay2 = 1500;
-if (window.screen.width < 750) {
-    delay = 1500;
-    delay2 = 5000;
-}
 
-let touchstartX = 0;
-let touchstartY = 0;
-let touchendX = 0;
-let touchendY = 0;
-
-var touch = function (event) {
-    touchstartX = event.changedTouches[0].screenX;
-    touchstartY = event.changedTouches[0].screenY;
-}
-var end = function (event) {
-    touchendX = event.changedTouches[0].screenX;
-    touchendY = event.changedTouches[0].screenY;
-    handleGesture();
-}
-
-window.addEventListener('touchstart', touch, false);
-
-window.addEventListener('touchend', end, false);
-
-
-function handleGesture() {
-    if (touchendY >= touchstartY) {
-        goBack();
-    }
-
-    if (touchendY <= touchstartY) {
-        if (onPart == 0) {
-            if (selectAll(".textFade1").style("opacity") == 1) {
-                onPart = 1;
-            }
-        }
-        if (curr == 1 && onPart == 1) {
-            fadeText(onPart);
-            if (selectAll(".textFade2").style("opacity") == 1) {
-                onPart = 2;
-            }
-
-        } else if (curr == 1 && onPart == 2) {
-            fadeText(onPart);
-            if (selectAll(".textFade3").style("opacity") > 0.5) {
-                onPart = 3;
-                something();
-            }
-
-        } else if (onPart == 3) {
-            if (selectAll(".textFade4").style("opacity") == 1) {
-                onPart = 4;
-            }
-
-        } else if (curr == 2 && onPart == 4) {
-            fadeText(onPart);
+var fadegrey = (function () {
+    var executed = false;
+    return function () {
+        if (!executed) {
             selectAll(".g-cstyle0")
                 .transition()
                 .delay(1000)
                 .style("transition", "2s")
                 .style("color", "#4D4D4D");
-            selectAll(".article")
-                .classed("fadeblack", true)
+        }
+    };
+})();
 
+var fadePoints = (function () {
+    var executed = false;
+    return function () {
+        if (!executed) {
+            executed = true;
             selectAll('.grey_point')
                 .each(function (d) {
-                    var vary = (Math.random() * 7000) + 3000;
+                    var vary = (Math.random() * 6000) + 1000;
                     select(this)
                         .transition()
                         .delay(vary)
+                        .duration(500)
+                        .attr("fill", "#ff073a")
+                        .transition()
                         .duration(500)
                         .attr("fill", "#4D4D4D")
                 })
-            if (selectAll(".dis").style("color") == "rgb(77, 77, 77)") {
-                setTimeout(() => { onPart = 5; }, delay)
-            }
+        }
+    };
+})();
 
-        } else if (curr == 2 && onPart == 5) {
-            fadeText(onPart);
-
+var fadePoints2 = (function () {
+    var executed = false;
+    return function () {
+        if (!executed) {
+            executed = true;
             selectAll('.grey_point_2')
                 .each(function (d) {
-                    var vary = (Math.random() * 7000) + 3000;
+                    var vary = (Math.random() * 6000) + 1000;
                     select(this)
                         .transition()
                         .delay(vary)
                         .duration(500)
-                        .style("transition", "visibility 0s linear 0s, opacity 400ms")
-                        .style("fill", "#4D4D4D")
-                })
-            if (selectAll(".textFade6").style("opacity") == 1) {
-                setTimeout(() => { finish() }, delay2);
-            }
-
-        }
-    }
-}
-var loading = false;
-
-var handle = function (e) { if (!loading) { loading = true; animationInstruct(e); loading = false } }
-
-
-function animationInstruct(e) {
-    if (e.deltaY > 0) {
-        //scroll down
-
-        if (onPart == 0) {
-            if (selectAll(".textFade1").style("opacity") == 1) {
-                onPart = 1;
-            }
-        }
-        if (curr == 1 && onPart == 1) {
-            fadeText(onPart);
-            if (selectAll(".textFade2").style("opacity") == 1) {
-                setTimeout(() => { onPart = 2 }, delay);
-            }
-
-        } else if (curr == 1 && onPart == 2) {
-            fadeText(onPart);
-            if (selectAll(".textFade3").style("opacity") > 0.5) {
-                onPart = 3;
-                something();
-            }
-
-        } else if (onPart == 3) {
-            if (selectAll(".textFade4").style("opacity") == 1) {
-                onPart = 4;
-            }
-
-        } else if (curr == 2 && onPart == 4) {
-            fadeText(onPart);
-            selectAll(".g-cstyle0")
-                .transition()
-                .delay(1000)
-                .style("transition", "2s")
-                .style("color", "#4D4D4D");
-            selectAll(".article")
-                .classed("fadeblack", true)
-
-            selectAll('.grey_point')
-                .each(function (d) {
-                    var vary = (Math.random() * 7000) + 3000;
-                    select(this)
+                        .attr("fill", "#ff073a")
                         .transition()
-                        .delay(vary)
                         .duration(500)
                         .attr("fill", "#4D4D4D")
                 })
-            if (selectAll(".dis").style("color") == "rgb(77, 77, 77)") {
-                setTimeout(() => { onPart = 5; }, delay)
-            }
-
-        } else if (curr == 2 && onPart == 5) {
-            fadeText(onPart);
-
-            selectAll('.grey_point_2')
-                .each(function (d) {
-                    var vary = (Math.random() * 7000) + 3000;
-                    select(this)
-                        .transition()
-                        .delay(vary)
-                        .duration(500)
-                        .style("transition", "visibility 0s linear 0s, opacity 400ms")
-                        .style("fill", "#4D4D4D")
-                })
-            if (selectAll(".textFade6").style("opacity") == 1) {
-                setTimeout(() => { finish() }, delay2);
-            }
-
         }
+    };
+})();
 
-    } else if (e.deltaY < 0) {
-        goBack();
+
+
+var handle = function (e) {
+    if (!loading) {
+        loading = true;
+        animationInstruct(e);
+        loading = false;
     }
-
-
 }
-
-window.addEventListener('wheel', handle);
-
-
 
 function enable(_callback) {
     // do some asynchronous work
@@ -246,15 +143,7 @@ function fadeText(onPart) {
 
 
 
-function before(index, sections) {
-    for (var i = 1; i < 10; i++) {
-        selectAll(".textFade" + i)
-            .classed("m-fadeIn", false)
-            .classed("m-fadeOut", true);
-    }
-}
-
-var something = (function () {
+var nextSlide = (function () {
     if (canGo) {
         canGo = false;
         $.scrollify.enable();
@@ -273,18 +162,182 @@ var finish = (function () {
             selectAll(".article")
                 .classed("fadeblack", false)
             executed = true;
-            $.scrollify.enable();
-            $.scrollify.move(3);
+            enable(function () {
+                $.scrollify.move(3);
+            })
         }
     };
 })();
 
+var touch = function (event) {
+    touchstartX = event.changedTouches[0].screenX;
+    touchstartY = event.changedTouches[0].screenY;
+}
+
+window.addEventListener('touchstart', touch, false);
+
+var end = function (event) {
+    touchendX = event.changedTouches[0].screenX;
+    touchendY = event.changedTouches[0].screenY;
+    handleGesture();
+}
+
+window.addEventListener('touchend', end, false);
 
 
-function checkInstant(index, sections) {
+function handleGesture() {
+    if (touchendY >= touchstartY) {
+        goBack();
+    }
+
+    if (touchendY <= touchstartY) {
+
+        if (onPart == 0) {
+
+            if (selectAll(".textFade1").style("opacity") == 1) {
+                onPart = 1;
+            }
+
+        }
+
+        if (curr == 1 && onPart == 1) {
+
+            fadeText(onPart);
+
+            if (selectAll(".textFade2").style("opacity") == 1) {
+                onPart = 2;
+            }
+
+        } else if (curr == 1 && onPart == 2) {
+
+            fadeText(onPart);
+
+            if (selectAll(".textFade3").style("opacity") > 0.5) {
+                onPart = 3;
+                nextSlide();
+            }
+
+        } else if (onPart == 3) {
+
+            if (selectAll(".textFade4").style("opacity") == 1) {
+                onPart = 4;
+            }
+
+        } else if (curr == 2 && onPart == 4) {
+
+            fadeText(onPart);
+
+            fadegrey();
+
+            selectAll(".article")
+                .classed("fadeblack", true)
+
+            fadePoints()
+
+            if (selectAll(".dis").style("color") == "rgb(77, 77, 77)") {
+                setTimeout(() => { onPart = 5; }, delay)
+            }
+
+        } else if (curr == 2 && onPart == 5) {
+
+            fadeText(onPart);
+
+            fadePoints2()
+
+            if (selectAll(".textFade6").style("opacity") == 1) {
+                setTimeout(() => { finish() }, delay2);
+            }
+
+        }
+    }
+}
+
+function animationInstruct(e) {
+    if (e.deltaY > 0) {
+        //scroll down
+
+        if (onPart == 0) {
+
+            if (selectAll(".textFade1").style("opacity") == 1) {
+                onPart = 1;
+            }
+
+        }
+        
+        if (curr == 1 && onPart == 1) {
+            fadeText(onPart);
+
+            if (selectAll(".textFade2").style("opacity") == 1) {
+                setTimeout(() => { onPart = 2 }, delay);
+            }
+
+        } else if (curr == 1 && onPart == 2) {
+            fadeText(onPart);
+
+            if (selectAll(".textFade3").style("opacity") > 0.5) {
+                onPart = 3;
+                nextSlide();
+            }
+
+        } else if (onPart == 3) {
+            if (selectAll(".textFade4").style("opacity") == 1) {
+                onPart = 4;
+            }
+
+        } else if (curr == 2 && onPart == 4) {
+            fadeText(onPart);
+
+            fadegrey();
+
+            selectAll(".article")
+                .classed("fadeblack", true)
+
+            fadePoints();
+
+            if (selectAll(".dis").style("color") == "rgb(77, 77, 77)") {
+                setTimeout(() => { onPart = 5; }, delay)
+            }
+
+        } else if (curr == 2 && onPart == 5) {
+            fadeText(onPart);
+
+            fadePoints2();
+
+            if (selectAll(".textFade6").style("opacity") == 1) {
+                setTimeout(() => { finish() }, delay2);
+            }
+
+        }
+
+    } else if (e.deltaY < 0) {
+        goBack();
+    }
+
+
+}
+
+window.addEventListener('wheel', handle);
+
+
+
+
+
+
+
+function before(index, sections) {
+    for (var i = 1; i < 10; i++) {
+        selectAll(".textFade" + i)
+            .classed("m-fadeIn", false)
+            .classed("m-fadeOut", true);
+    }
+}
+
+function after(index, sections) {
+
     if (index == 0) {
         $.scrollify.enable();
     }
+
     if (index == 1) {
         selectAll(".textFade1")
             .classed("m-fadeIn", true)
@@ -300,30 +353,28 @@ function checkInstant(index, sections) {
         onPart = 0;
         $.scrollify.disable();
     }
+
     if (index == 2) {
         selectAll(".textFade4")
             .classed("m-fadeIn", true)
             .classed("m-fadeOut", false);
         onPart = 3;
-        console.log("got here");
         $.scrollify.disable();
     }
 
     if (index == 3) {
-        console.log(("hi"));
-        window.removeEventListener("wheel", handle);
-        window.removeEventListener("touchstart", touch);
-        window.removeEventListener("touchend", end);
+        $.scrollify.destroy();
+        goBack = 1;
+        window.removeEventListener("wheel", handle, false);
+        window.removeEventListener("touchstart", touch, false);
+        window.removeEventListener("touchend", end, false);
         onPart = 4;
-        $.scrollify.disable();
         cleanup();
     }
 
 }
 
 function cleanup() {
-    setTimeout(() => { canGo = false; }, 2000);
- 
     document.getElementsByTagName("body")[0].style = "overflow: visible !important;";
     document.getElementsByTagName("body")[0].style = "background-color:white;";
 
