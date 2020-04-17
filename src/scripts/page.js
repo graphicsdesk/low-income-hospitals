c3b91e51b4df3e214dcf021b811e0e7d790c0bfd
Jason Kao <jason.kao@columbia.edu>
2020-04-17 16:34:11 -0400

    moved resizer to hoistarticle

--- src/scripts/page.js
+++ src/scripts/page.js
@@ -72,5 +72,8 @@ function ready(timestamp) {
 }
 
 // Initialize our ready() function.
-if (isOnSpectatorPage)
+if (isOnSpectatorPage) {
   window.requestAnimationFrame(ready);
+} else {
+  resizer();
+}
