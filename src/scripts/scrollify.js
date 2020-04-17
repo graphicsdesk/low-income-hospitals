5aab0800c508925a8a189348958256a503eccfeb
HongSenDu <hongsendu@gmail.com>
2020-04-17 05:52:03 -0400

    adjust

--- src/scripts/scrollify.js
+++ src/scripts/scrollify.js
@@ -39,96 +39,99 @@ let touchstartY = 0;
 let touchendX = 0;
 let touchendY = 0;
 
-
-window.addEventListener('touchstart', function (event) {
+var touch = function (event) {
     touchstartX = event.changedTouches[0].screenX;
     touchstartY = event.changedTouches[0].screenY;
-}, false);
-
-window.addEventListener('touchend', function (event) {
+}
+var end = function (event) {
     touchendX = event.changedTouches[0].screenX;
     touchendY = event.changedTouches[0].screenY;
     handleGesture();
-}, false);
+}
+
+window.addEventListener('touchstart', touch, false);
+
+window.addEventListener('touchend', end, false);
 
 
-    function handleGesture() {
-        if (touchendY >= touchstartY) {
-            goBack();
+function handleGesture() {
+    if (touchendY >= touchstartY) {
+        goBack();
+    }
+
+    if (touchendY <= touchstartY) {
+        if (onPart == 0) {
+            if (selectAll(".textFade1").style("opacity") == 1) {
+                onPart = 1;
+            }
         }
-
-        if (touchendY <= touchstartY) {
-            if (onPart == 0) {
-                if (selectAll(".textFade1").style("opacity") == 1) {
-                    onPart = 1;
-                }
+        if (curr == 1 && onPart == 1) {
+            fadeText(onPart);
+            if (selectAll(".textFade2").style("opacity") == 1) {
+                onPart = 2;
             }
-            if (curr == 1 && onPart == 1) {
-                fadeText(onPart);
-                if (selectAll(".textFade2").style("opacity") == 1) {
-                    onPart = 2;
-                }
 
-            } else if (curr == 1 && onPart == 2) {
-                fadeText(onPart);
-                if (selectAll(".textFade3").style("opacity") > 0.5) {
-                    onPart = 3;
-                    something();
-                }
-
-            } else if (onPart == 3) {
-                if (selectAll(".textFade4").style("opacity") == 1) {
-                    onPart = 4;
-                }
-
-            } else if (curr == 2 && onPart == 4) {
-                fadeText(onPart);
-                selectAll(".g-cstyle0")
-                    .transition()
-                    .delay(1000)
-                    .style("transition", "2s")
-                    .style("color", "#4D4D4D");
-                selectAll(".article")
-                    .classed("fadeblack", true)
-
-                selectAll('.grey_point')
-                    .each(function (d) {
-                        var vary = (Math.random() * 7000) + 3000;
-                        select(this)
-                            .transition()
-                            .delay(vary)
-                            .duration(500)
-                            .attr("fill", "#4D4D4D")
-                    })
-                if (selectAll(".dis").style("color") == "rgb(77, 77, 77)") {
-                    setTimeout(() => { onPart = 5; }, delay)
-                }
-
-            } else if (curr == 2 && onPart == 5) {
-                fadeText(onPart);
-
-                selectAll('.grey_point_2')
-                    .each(function (d) {
-                        var vary = (Math.random() * 7000) + 3000;
-                        select(this)
-                            .transition()
-                            .delay(vary)
-                            .duration(500)
-                            .style("transition", "visibility 0s linear 0s, opacity 400ms")
-                            .style("fill", "#4D4D4D")
-                    })
-                if (selectAll(".textFade6").style("opacity") == 1) {
-                    setTimeout(() => { finish() }, delay2);
-                }
-
+        } else if (curr == 1 && onPart == 2) {
+            fadeText(onPart);
+            if (selectAll(".textFade3").style("opacity") > 0.5) {
+                onPart = 3;
+                something();
             }
+
+        } else if (onPart == 3) {
+            if (selectAll(".textFade4").style("opacity") == 1) {
+                onPart = 4;
+            }
+
+        } else if (curr == 2 && onPart == 4) {
+            fadeText(onPart);
+            selectAll(".g-cstyle0")
+                .transition()
+                .delay(1000)
+                .style("transition", "2s")
+                .style("color", "#4D4D4D");
+            selectAll(".article")
+                .classed("fadeblack", true)
+
+            selectAll('.grey_point')
+                .each(function (d) {
+                    var vary = (Math.random() * 7000) + 3000;
+                    select(this)
+                        .transition()
+                        .delay(vary)
+                        .duration(500)
+                        .attr("fill", "#4D4D4D")
+                })
+            if (selectAll(".dis").style("color") == "rgb(77, 77, 77)") {
+                setTimeout(() => { onPart = 5; }, delay)
+            }
+
+        } else if (curr == 2 && onPart == 5) {
+            fadeText(onPart);
+
+            selectAll('.grey_point_2')
+                .each(function (d) {
+                    var vary = (Math.random() * 7000) + 3000;
+                    select(this)
+                        .transition()
+                        .delay(vary)
+                        .duration(500)
+                        .style("transition", "visibility 0s linear 0s, opacity 400ms")
+                        .style("fill", "#4D4D4D")
+                })
+            if (selectAll(".textFade6").style("opacity") == 1) {
+                setTimeout(() => { finish() }, delay2);
+            }
+
         }
     }
+}
 var loading = false;
+
+var handle = function (e) { if (!loading) { loading = true; animationInstruct(e); loading = false } }
 
 window.addEventListener('wheel', handle);
 
-var handle = function (e) { if (!loading) { loading = true; animationInstruct(e); loading = false } }
 function animationInstruct(e) {
     if (e.deltaY > 0) {
         //scroll down
@@ -314,8 +317,10 @@ function checkInstant(index, sections) {
 }
 
 function cleanup() {
-    canGo = false;
+    setTimeout(() => { canGo = false; }, 2000);
     window.removeEventListener("window", handle);
+    window.removeEventListener("window", touch);
+    window.removeEventListener("window", end);
     document.getElementsByTagName("body")[0].style = "overflow: visible !important;";
     selectAll(".makeInv")
         .style("display", "none")
